64db4cec816b83f1afffcb4c073991530a65028d
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-25 12:17:29 -0300

    Using IComparable

---
+++ Comparison/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison.Entities
+{
+    internal class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return Name + ", "+ Price.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product other)
+        {
+            return this.Name.ToUpper().CompareTo(other.Name.ToUpper()); 
+        }
+    }
+}
--- Comparison/Program.cs
+++ Comparison/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comparison.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace Comparison
     {
         static void Main(string[] args)
         {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort();
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+
+            }
+            Console.ReadKey();
         }
     }
 }
